24437548c846334cce9f22d504add77ebbd1981a
Sean Amoroso <sean.amoroso@bentley.com>
2019-08-23 06:31:52 -0400

    docker POC sample program

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './compiled/App.css';
+import React, {  Suspense, useState, useEffect } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import Spinner from 'react-bootstrap/Spinner';
 
-class App extends Component {
-  constructor() {
-    super();
 
-    this.state = {};
+import './App.css';
+
+const App = () => {
+  const [people, setPeople] = useState([]);
+  const [activePerson, setActivePerson] = useState({});
+  const [films, setFilms] = useState([]);
+  const [activeFilm, setActiveFilm] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const Loading = () => {
+    return <Spinner animation="grow" />
   }
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState(res))
-      .catch(console.error);
+ useEffect(() => {
+    callSongs().then((resp) => {
+      console.log('what is data', resp);
+      setPeople(resp.results); 
+      setIsLoading(false);
+    }); 
+  }, []);
+ 
+
+  const callSongs = async () => { 
+    const response = await fetch('/api/songs'); 
+    if (response.status !== 200) {
+      throw Error(response ? response.message : 'No data');
+    } 
+    // only proceed once promise is resolved
+    return await response.json();  
+  }; 
+
+  const getFilms = async (p) => {  
+    setActivePerson(p);
+    setFilms(p.films); 
+    handleShow();
+    const response = await fetch(p.films[0]);
+    const film = await response.json();
+    console.log(film);
+    setActiveFilm(film); 
   }
-
-  callApi = async () => {
-    const resp = await fetch('/api');
-
-    window._resp = resp;
-
-    let text = await resp.text();
-
-    let data = null;
-    try {
-      data = JSON.parse(text); // cannot call both .json and .text - await resp.json();
-    } catch (e) {
-      console.err(`Invalid json\n${e}`);
-    }
-
-    if (resp.status !== 200) {
-      throw Error(data ? data.message : 'No data');
-    }
-
-    return data;
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{this.state.message || 'No message'}</p>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+    {isLoading ? <Loading /> :
+    
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Birth</th>
+          <th>Films</th> 
+        </tr>
+      </thead>
+      <tbody>
+        {people.map((p) => {
+            return <tr key={p.name}> 
+              <td>{p.name}</td>
+              <td>{p.birth_year}</td>
+              <td><div className='films' onClick={() => getFilms(p)}>Films</div></td> 
+            </tr>
+          })} 
+      </tbody>
+    </Table>       
 }
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Films: {activePerson.name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+      {isLoading && <Loading />}
+      {!isLoading &&
+        <>
+        {activeFilm.title} <br />
+        {activeFilm.opening_crawl} <br />
+        Total number of films: {films.length}
+        </>
+        }
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button> 
+      </Modal.Footer>
+    </Modal>
+    </div>
+  ); 
+}
+ 
 
 export default App;
--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const path = require('path');
+var async = require('express-async-await');
+var fetch = require('node-fetch');
 
 // Constants
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
-
+const songRouter = express.Router();
 const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build');
 
 // App
@@ -15,19 +17,60 @@ const app = express();
 // Static files
 app.use(express.static(CLIENT_BUILD_PATH));
 
-// API
-app.get('/api', (req, res) => {
-  res.set('Content-Type', 'application/json');
-  let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
-  };
-  res.send(JSON.stringify(data, null, 2));
+// // API
+// app.get('/api', (req, res) => {
+//   res.set('Content-Type', 'application/json');
+//   let data = {
+//     message: 'Hello world, death!!!!'
+//   };
+//   res.send(JSON.stringify(data, null, 2));
+// });
+
+songRouter.route('/songs').get((req, res) => {
+  const apiUrl2 = 'https://swapi.co/api/people';
+
+  fetch(apiUrl2, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+    body: JSON.stringify({ start: 0, end: 35 })
+  })
+    .then(res => res.json())
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.send({ error: 'bad' });
+      //	res.redirect('/error');
+    });
 });
+app.use('/api', songRouter);
+// app.get('/songs', (req, res) => {
+// 	//build api URL with user zip
+
+// 	const apiUrl = 'https://musicdemons.com/api/v1/song';
+//    const apiUrl2 = 'https://swapi.co/api/people';
+
+// 	fetch(apiUrl2, {
+//      method: 'POST',
+//     headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+//     body: JSON.stringify({start: 0, end: 35})
+//   })
+// 	.then(res => res.json())
+// 	.then(data => {
+// 		res.send(JSON.stringify(data, null, 2));
+// 	})
+// 	.catch(err => {
+//     console.log('nope', err);
+//     res.send({ error: "FUK"});
+// 	//	res.redirect('/error');
+// 	});
+
+// })
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
-  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
-});
+// app.get('*', function(request, response) {
+//   response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
+// });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
